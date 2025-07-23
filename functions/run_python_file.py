2f26d2392956d1dd819491e35d9162503e2e7b3f
Amir Zahirovic <amirzahirovic0@gmail.com>
2025-07-23 16:49:41 +0200

    After looking at solutions files, I have reworked some of the checks and return method

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -15,16 +15,26 @@ def run_python_file(working_directory, file_path, args=[]):
         return f'Error: "{file_path}" is not a Python file.'
     
     try:
-        command_list = ["python"]
-        command_list.append(file_path)
+        command_list = ["python", file_path]
         if args:
             command_list += args
-        completed_process = subprocess.run(command_list, cwd=abs_working_directory_path, capture_output=True, timeout=30.0, text=True)
-        print(f'STDOUT: {completed_process.stdout}')
-        print(f'STDERR: {completed_process.stderr}')
+        completed_process = subprocess.run(
+            command_list, 
+            cwd=abs_working_directory_path, 
+            capture_output=True, 
+            timeout=30.0, 
+            text=True)
+        
+        output = []
+        if completed_process.stdout:
+            output.append(f"STDOUT:\n{completed_process.stdout}")
+        if completed_process.stderr:
+            output.append(f"STDERR:\n{completed_process.stderr}")
         if completed_process.returncode != 0:
             print(f'Process exited with code {completed_process.returncode}')
-        if not completed_process.stdout:
+        if output:
+            return "\n".join(output)
+        else:
             return f'No output produced'
     except Exception as e:
         return f"Error: Cannot execute the command due to : {e}"
--- tests.py
+++ tests.py
@@ -3,22 +3,26 @@ from functions.get_file_content import get_file_content
 from functions.write_file import write_file
 from functions.run_python_file import run_python_file
 
-print("Test 1 --- Start")
-print(run_python_file("calculator","main.py"))
-print("Test 1 --- End")
+def test():
+    print("Test 1 --- Start")
+    print(run_python_file("calculator","main.py"))
+    print("Test 1 --- End")
 
-print("Test 2 --- Start")
-print(run_python_file("calculator","main.py", ["3 + 5"]))
-print("Test 2 --- End")
+    print("Test 2 --- Start")
+    print(run_python_file("calculator","main.py", ["3 + 5"]))
+    print("Test 2 --- End")
 
-print("Test 3 --- Start")
-print(run_python_file("calculator","tests.py"))
-print("Test 3 --- End")
+    print("Test 3 --- Start")
+    print(run_python_file("calculator","tests.py"))
+    print("Test 3 --- End")
 
-print("Test 4 --- Start")
-print(run_python_file("calculator","../main.py"))
-print("Test 4 --- End")
+    print("Test 4 --- Start")
+    print(run_python_file("calculator","../main.py"))
+    print("Test 4 --- End")
 
-print("Test 5 --- Start")
-print(run_python_file("calculator","nonexistent.py"))
-print("Test 5 --- End")
+    print("Test 5 --- Start")
+    print(run_python_file("calculator","nonexistent.py"))
+    print("Test 5 --- End")
+
+if __name__ == "__main__":
+    test()
